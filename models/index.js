30c65d6c4e107c69c76ea69354185c6db75aa90a
Dylan Thomson <djthomson88@gmail.com>
2019-02-16 15:59:38 -0500

    Change env variable name

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ const Sequelize = require("sequelize");
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
+console.log(config);
 const db = {};
 let sequelize;
 if (config.jawsDB) {
