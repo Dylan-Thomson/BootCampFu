7f3d99d52c9951dd81c6b268783bcc2285b1eede
Rahul Raj <rahulrajshopping@icloud.com>
2019-02-17 12:01:43 -0500

    added multiple styles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,27 +2,15 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/", (req, res) => {
+    res.render("index", {
+      style: "styles.css"
     });
   });
 
   app.get("/questions", (req, res) => {
-    res.render("question-list", {});
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
+    res.render("question-list", {
+      style: "question-list.css"
     });
   });
 };
