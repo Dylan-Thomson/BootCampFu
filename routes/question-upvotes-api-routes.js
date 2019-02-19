e862d3d1a787a95f94ffa0a7867739198f55b209
Dylan Thomson <djthomson88@gmail.com>
2019-02-19 14:55:43 -0500

    User can undo upvotes/downvotes by upvoting/downvoting again

--- routes/question-upvotes-api-routes.js
+++ routes/question-upvotes-api-routes.js
@@ -3,18 +3,46 @@ const db = require("../models");
 module.exports = function(app) {
   // Requires UserId in req.body
   app.post("/api/questions/:questionId/upvotes", (req, res) => {
-    isUpvotedByUser(req.body.UserId, req.params.questionId).then(isLiked => {
-      if (isLiked) {
+    hasUserVoted(
+      req.body.UserId,
+      req.params.questionId,
+      "QuestionUpvotes"
+    ).then(isUpvoted => {
+      if (isUpvoted) {
         res.send(false);
       } else {
-        req.body.QuestionId = req.params.questionId;
-        db.QuestionUpvotes.create(req.body).then(() => {
-          db.Question.update(
-            { score: db.sequelize.literal("score + 1") },
-            { where: { id: req.params.questionId } }
-          ).then(dbQuestion => {
-            res.json(dbQuestion);
-          });
+        hasUserVoted(
+          req.body.UserId,
+          req.params.questionId,
+          "QuestionDownvotes"
+        ).then(isDownvoted => {
+          if (isDownvoted) {
+            // remove from QuestionDownVotes, update score
+            db.QuestionDownvotes.destroy({
+              where: {
+                QuestionId: req.params.questionId,
+                UserId: req.body.UserId
+              }
+            }).then(() => {
+              db.Question.update(
+                { score: db.sequelize.literal("score + 1") },
+                { where: { id: req.params.questionId } }
+              ).then(dbQuestion => {
+                res.json(dbQuestion);
+              });
+            });
+          } else {
+            // add to QuestionUpvotes, update score
+            req.body.QuestionId = req.params.questionId;
+            db.QuestionUpvotes.create(req.body).then(() => {
+              db.Question.update(
+                { score: db.sequelize.literal("score + 1") },
+                { where: { id: req.params.questionId } }
+              ).then(dbQuestion => {
+                res.json(dbQuestion);
+              });
+            });
+          }
         });
       }
     });
@@ -31,11 +59,13 @@ module.exports = function(app) {
   });
 
   // TODO: Migrate to export file, probably in Models somewhere
-  function isUpvotedByUser(userId, questionId) {
-    return db.QuestionUpvotes.count({
-      where: { UserId: userId, QuestionId: questionId }
-    }).then(count => {
-      return count !== 0;
-    });
+  function hasUserVoted(userId, questionId, model) {
+    return db[model]
+      .count({
+        where: { UserId: userId, QuestionId: questionId }
+      })
+      .then(count => {
+        return count !== 0;
+      });
   }
 };
