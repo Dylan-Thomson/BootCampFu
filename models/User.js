1151f6fe519927fcd895a00baae010b01d00eee4
Dylan Thomson <djthomson88@gmail.com>
2019-02-19 12:58:36 -0500

    Set username and email to unique, install sequelize-cli

--- models/User.js
+++ models/User.js
@@ -2,12 +2,14 @@ module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", {
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: { isEmail: true }
+      validate: { isEmail: true },
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
