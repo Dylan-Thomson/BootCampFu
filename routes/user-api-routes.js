a129129ae2266dea75332782a1b05fd49b49b0e2
Dylan Thomson <djthomson88@gmail.com>
2019-02-21 14:14:32 -0500

    userController

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,39 +1,9 @@
-const db = require("../models");
+const userController = require("../controllers/userController");
 
 module.exports = function(app) {
-  app.post("/api/users", (req, res) => {
-    db.User.create(req.body).then(dbUser => {
-      res.json(dbUser);
-    });
-  });
+  app.get("/api/users", userController.getUsers);
 
-  app.get("/api/users", (req, res) => {
-    db.User.findAll({
-      include: [db.Question],
-      attributes: ["username", "email", "status"]
-    }).then(dbUser => {
-      res.json(dbUser);
-    });
-  });
+  app.get("/api/users/:id", userController.getUserById);
 
-  app.get("/api/users/:id", (req, res) => {
-    db.User.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Question]
-    }).then(dbUser => {
-      res.json(dbUser);
-    });
-  });
-
-  app.delete("/api/users/:id", (req, res) => {
-    db.User.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(dbUser => {
-      res.json(dbUser);
-    });
-  });
+  app.delete("/api/users/:id", userController.deleteUser);
 };
