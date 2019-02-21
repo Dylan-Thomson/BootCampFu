3c2132a8681da66579adbf5292ede448c168f720
Dylan Thomson <djthomson88@gmail.com>
2019-02-21 13:55:15 -0500

    answerController

--- routes/answer-api-routes.js
+++ routes/answer-api-routes.js
@@ -1,25 +1,28 @@
-const db = require("../models");
+const answerController = require("../controllers/answerController");
 
 module.exports = function(app) {
-  app.post("/api/questions/:questionId/answers", (req, res) => {
-    if (req.user) {
-      req.body.QuestionId = req.params.questionId;
-      req.body.UserId = req.user.id;
-      db.Answer.create(req.body).then(dbAnswer => {
-        res.json(dbAnswer);
-      });
-    } else {
-      res.redirect("/signin");
-    }
-  });
-  app.get("/api/questions/:questionId/answers", (req, res) => {
-    db.Answer.findAll({
-      where: {
-        QuestionId: req.params.questionId
-      },
-      include: [db.User, db.Question]
-    }).then(dbAnswer => {
-      res.json(dbAnswer);
-    });
-  });
+  app.post("/api/questions/:questionId/answers", answerController.postAnswer);
+  // app.post("/api/questions/:questionId/answers", (req, res) => {
+  //   if (req.user) {
+  //     req.body.QuestionId = req.params.questionId;
+  //     req.body.UserId = req.user.id;
+  //     db.Answer.create(req.body).then(dbAnswer => {
+  //       res.json(dbAnswer);
+  //     });
+  //   } else {
+  //     res.redirect("/signin");
+  //   }
+  // });
+
+  app.get("/api/questions/:questionId/answers", answerController.getAnswers);
+  // app.get("/api/questions/:questionId/answers", (req, res) => {
+  //   db.Answer.findAll({
+  //     where: {
+  //       QuestionId: req.params.questionId
+  //     },
+  //     include: [db.User, db.Question]
+  //   }).then(dbAnswer => {
+  //     res.json(dbAnswer);
+  //   });
+  // });
 };
